895b1cfc2af9672bdfc995834ef15b1581563b17
stevensonmat2 <stevensonmat2@gmail.com>
2023-04-12 13:18:12 -0700

    hw1 update

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,38 +1,36 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
+const element = document.querySelector("#validation-message");
+const message = document.createElement("p");
+message.textContent = "No input";
+message.style.color = "black";
+element.append(message);
 
-elem.addEventListener('input', handleInput);
+elem.addEventListener("input", handleInput);
 
 function handleInput(e) {
-    let input = e.target.value;
-    console.log(input)
-    let element = document.querySelector("#validation-message");
-    let message = document.createElement("p");
+  let input = e.target.value;
 
-    if (input < 0) {
-        message.textContent = "Invalid input! Postive numbers only"
-        message.style.color = "black";
+  if (input < 0) {
+    message.textContent = "Invalid input! Postive numbers only";
+    message.style.color = "black";
+  } else {
+    if (checkForPalindrome(input)) {
+      message.textContent = "This is a palindrome!";
+      message.style.color = "green";
+    } else {
+      message.textContent = "This is not a palindrome!";
+      message.style.color = "red";
     }
+  }
 
-    else {
-        if (checkForPalindrome(input)) {
-            message.textContent = "This is a palindrome!"
-            message.style.color = "green";
-        }
-
-        else {
-            message.textContent = "This is not a palindrome!"
-            message.style.color = "red";
-        }
-    }
-
-    element.append(message);
+  element.append(message);
 }
 
 function checkForPalindrome(number) {
-    let numberString = number.toString();
-    let splitString = numberString.split("");
-    let reverseSplit = splitString.reverse();
-    let reversedString = reverseSplit.join("");
+  let numberString = number.toString();
+  let splitString = numberString.split("");
+  let reverseSplit = splitString.reverse();
+  let reversedString = reverseSplit.join("");
 
-    return numberString == reversedString;
+  return numberString == reversedString;
 }
